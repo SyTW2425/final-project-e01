6469994c263c8fec5531dae5020f5eb8e12b0d17
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-10-27 21:35:59 +0100

    JWT Validations done

--- packages/backend/src/Routers/users.ts
+++ packages/backend/src/Routers/users.ts
@@ -1,26 +1,31 @@
-/**
- * Project Mangement API
- * Author: NoexDev
- * Date: 17/08/2024
- * Description: This file contains the express router manager for the Users
- */
-
+import 'dotenv/config';
 import Express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import jwtMiddleware from '../Middleware/authMiddleware.js';
+
 import { User } from '../Models/User.js';
+
+const JWT_SECRET  = process.env.JWT_SECRET || 'CHILINDRINA';
 
 export const usersRouter = Express.Router();
 
-usersRouter.get('/', async (req, res) => {
+usersRouter.get('/', jwtMiddleware, async (req, res) => {
   try {
     const query = searchUsersChecks(req);
     if (typeof query === 'string') {
       res.status(400).send(query);
       return;
-    }    
+    }
     const author = req.body.author;
-    const usersRaw = await User.find(query).select('-password');
-    const authorUser = await User.find({ "username": author });
-    
+
+    const usersRaw = await User.find(query)
+    .select('-password')
+    .select('-projects')
+    .select('-__v')
+    .select('-_id');
+    const authorUser = await User.find({ username: author });
+
     if (!query || !authorUser) {
       res.status(404).send('Failed to search users!');
       return;
@@ -28,11 +33,14 @@ usersRouter.get('/', async (req, res) => {
 
     // Check if the author is in the same project as the users, to filter the email
     for (let user of usersRaw) {
-      let inSameProject : Boolean = authorUser[0].projects.some((project_author) => {
-        return user.projects.includes(project_author);
-      });
+      let inSameProject: Boolean = authorUser[0].projects.some(
+        (project_author) => {
+          return user.projects.includes(project_author);
+        },
+      );
       user.email = inSameProject ? user.email : '';
     }
+
     res.send(usersRaw);
   } catch (error) {
     res.status(500).send('Error searching users!');
@@ -42,22 +50,57 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/register', async (req, res) => {
   try {
     let { username, email, password } = req.body;
-    const user = new User({ username, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ 
+      username, 
+      email, 
+      password: hashedPassword
+    });
     await user.save();
-    res.send(user);
+
+    user.populate('-__v');
+    user.populate('-_id');
+
+    res.status(201).send(user);
   } catch (error) {
-    res.send('Error: ' + error);
+    res.status(500).send('Error: ' + error);
   }
 });
 
+usersRouter.post('/login', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const query = { $or: [{ username }, { email }] };
+
+    const user = await User.findOne(query);
+    if (!user) {
+      res.status(404).json({error: 'Authentication failed'});
+      return;
+    }
+   const passwordMatch = bcrypt.compareSync(password, user.password);
+    if (!passwordMatch) {
+      res.status(404).json({error: 'Authentication failed'});
+      return;
+    }
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: '1h'
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.json('Error: ' + error);
+  }
+});
 
 
 /**
  * This function checks the search users request
  */
-function searchUsersChecks(req: Express.Request) : object | string {
+function searchUsersChecks(req: Express.Request): object | string {
   const { username, email, author } = req.body;
-  const query : any = {};
+  const query: any = {};
   // Validations
   if (!username && !email) {
     return 'You must provide a username or email to search for users';
@@ -67,10 +110,10 @@ function searchUsersChecks(req: Express.Request) : object | string {
   }
   // Build the query
   if (username) {
-    query.username = { $regex: username, $options: 'i' };
+    query.username = { $regex: `^${username}`, $options: 'i' };
   }
   if (email) {
-    query.email = { $regex: email, $options: 'i' };
+    query.email = { $regex: `^${email}`, $options: 'i' };
   }
   return query;
 }
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,28 +2,33 @@ import 'dotenv/config';
 import express from 'express';
 import chalk from 'chalk';
 import { connect } from 'mongoose';
-
 import { usersRouter } from './Routers/users.js';
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/test';
 
-
 /**
  * Function to start the database connection
  * @returns void
  */
-function startDB() : void {
-  connect(MONGODB_URL).then(() => {
-    console.log(chalk.green('[Server_startdb] Connected to the database'));
-  }).catch(() => {
-    console.log(chalk.red('[Server_startdb] Something went wrong when conecting to the database'));
-    process.exit(-1);
-  });
+function startDB(): void {
+  connect(MONGODB_URL)
+    .then(() => {
+      console.log(chalk.green('[Server_startdb] Connected to the database'));
+    })
+    .catch(() => {
+      console.log(
+        chalk.red(
+          '[Server_startdb] Something went wrong when conecting to the database',
+        ),
+      );
+      process.exit(-1);
+    });
 }
 
 // Initialize the express server
 export const app = express();
+
 app.use(express.json());
 app.use('/user', usersRouter);
 console.log(chalk.green(`[Server_start] Server started at port ${PORT}!`));
